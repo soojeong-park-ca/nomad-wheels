505883dd00de1d3d35d629ce18bb41a532127049
Soojeong Park <soojeong.park.ca@gmail.com>
2023-05-31 12:40:55 -0700

    change background image for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
+import AppContext from "../store/app-context";
 import homeBgImg from "../assets/images/home-bg.png";
-import homeBgImgBig from "../assets/images/home-bg-big.png";
 
 export default function Home() {
+  const appCtx = useContext(AppContext);
+
   return (
     <section id="home" className="home">
       <img
         className="home__bg-img"
+        style={
+          appCtx.mobile
+            ? { objectPosition: "top" }
+            : { objectPosition: "50% 68%" }
+        }
         src={homeBgImg}
         alt="A mountain view from a van window"
       />
@@ -15,8 +23,8 @@ export default function Home() {
         You got the travel plans, we got the travel vans.
       </h1>
       <p className="paragraph">
-        Add adventure to your life by joining the #vanlife movement. Rent the
-        perfect van to make your perfect road trip.
+        Add adventure to your life by joining Nomad Wheel. Rent the perfect van
+        to make your perfect road trip.
       </p>
       <Link to="/vans" className="btn btn--orange margin-bottom-big">
         <div>Find your van</div>
