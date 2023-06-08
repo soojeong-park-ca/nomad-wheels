d8ace127525c656fd9eadd5eb75c168ce08a9104
Soojeong Park <soojeong.park.ca@gmail.com>
2023-06-07 23:12:07 -0700

    add responsive styles to Host Dashboard

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -41,65 +41,67 @@ export default function Dashboard() {
 
   return (
     <div className="host-dashboard">
-      <section className="host-dashboard__earnings">
-        <div className="app-padding-inline-default">
-          <CenteredMaxWidthBox>
-            <div className="host-dashboard__earnings-grid">
-              <div className="host-dashboard__earnings-info">
-                <h1 className="dashboard-heading-primary">
-                  Welcome back {userData.name}!
-                </h1>
-                <p>
-                  Income last <span>30 days</span>
-                </p>
-                <h2 className="dashboard-heading-secondary">$2,260</h2>
+      <div className="host-dashboard-desktop-layout">
+        <section className="host-dashboard__earnings">
+          <div className="app-padding-inline-default">
+            <CenteredMaxWidthBox>
+              <div className="host-dashboard__earnings-grid">
+                <div className="host-dashboard__earnings-info">
+                  <h1 className="dashboard-heading-primary">
+                    Welcome back {userData.name}!
+                  </h1>
+                  <p>
+                    Income last <span>30 days</span>
+                  </p>
+                  <h2 className="dashboard-heading-secondary">$2,260</h2>
+                </div>
+                <div className="detail-btn-box">
+                  <Link to="income" className="btn--detail">
+                    Details
+                  </Link>
+                </div>
               </div>
-              <div className="detail-btn-box">
-                <Link to="income" className="btn--detail">
+            </CenteredMaxWidthBox>
+          </div>
+        </section>
+
+        <section className="host-dashboard__reviews">
+          <div className="app-padding-inline-default">
+            <CenteredMaxWidthBox>
+              <div className="host-dashboard__reviews-grid">
+                <div className="review">
+                  <h2>Review score</h2>
+                  <div className="review-score">
+                    <i className="fa-solid fa-star icon-star"></i>
+                    <p>
+                      <span>5.0</span>/5
+                    </p>
+                  </div>
+                </div>
+                <Link to="reviews" className="btn--detail">
                   Details
                 </Link>
               </div>
-            </div>
-          </CenteredMaxWidthBox>
-        </div>
-      </section>
+            </CenteredMaxWidthBox>
+          </div>
+        </section>
 
-      <section className="host-dashboard__reviews">
-        <div className="app-padding-inline-default">
-          <CenteredMaxWidthBox>
-            <div className="host-dashboard__reviews-grid">
-              <div className="review">
-                <h2>Review score</h2>
-                <div className="review-score">
-                  <i className="fa-solid fa-star icon-star"></i>
-                  <p>
-                    <span>5.0</span>/5
-                  </p>
+        <section className="host-dashboard__vans">
+          <div className="app-padding-inline-default">
+            <CenteredMaxWidthBox>
+              <div className="host-dashboard__vans-box">
+                <div className="host-dashboard__vans-title">
+                  <h2>Your listed vans</h2>
+                  <Link to="vans" className="btn--detail">
+                    View all
+                  </Link>
                 </div>
+                <HostVansEls dataPromise={dataPromise} userData={userData} />
               </div>
-              <Link to="reviews" className="btn--detail">
-                Details
-              </Link>
-            </div>
-          </CenteredMaxWidthBox>
-        </div>
-      </section>
-
-      <section className="host-dashboard__vans">
-        <div className="app-padding-inline-default">
-          <CenteredMaxWidthBox>
-            <div className="host-dashboard__vans-box">
-              <div className="host-dashboard__vans-title">
-                <h2>Your listed vans</h2>
-                <Link to="vans" className="btn--detail">
-                  View all
-                </Link>
-              </div>
-              <HostVansEls dataPromise={dataPromise} userData={userData} />
-            </div>
-          </CenteredMaxWidthBox>
-        </div>
-      </section>
+            </CenteredMaxWidthBox>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
